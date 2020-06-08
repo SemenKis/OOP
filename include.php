2cd14b888c40c50875e1447fb3cb220083f54a77
Semen Kiselev <semyon.kiselyov.90@mail.ru>
2020-06-08 15:04:06 +0300

    php shop page

--- include.php
+++ include.php
@@ -5,13 +5,59 @@
  */
 class Shop {
 
-	public static $cost;
-	public static $product;
-	public static $id;
+	public static $products = [];
 
-	function __construct($cost, $product, $id) {
+	public function __construct($products) {
+		$this->product = $products;
+	}
+
+	public function makeProducts() {
+		$products = [
+			1 => ["id" => 1, "name" => "Микрофон"],
+			2 => ["id" => 2, "name" => "подставка"],
+			3 => ["id" => 3, "name" => "стойка"],
+			4 => ["id" => 4, "name" => "виски"],
+			5 => ["id" => 5, "name" => "кола"],
+		];
+
+		foreach ($products as $items => $values) {
+			echo "<div class='product'>";
+			echo "<div id='id'>{$products[$items]["id"]}</div>";
+			echo "<div class='item'>{$products[$items]["name"]}</div>";
+			echo "</div>";
+			echo "<br>";
+		}
 
 	}
+}
+
+// №5
+// class A {
+// 	public function foo() {
+// 		static $x = 0;
+// 		echo ++$x;
+// 	}
+// }
+
+// №5.2
+// class A {
+// 	public function foo() {
+// 		static $x = 0;
+// 		echo ++$x;
+// 	}
+// }
+
+// class B extends A {
+// }
+
+// №7
+class A {
+	public function foo() {
+		static $x = 0;
+		echo ++$x;
+	}
+}
+class B extends A {
 }
 
 ?>
--- index.php
+++ index.php
@@ -4,12 +4,36 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
 	<title>ООП git</title>
 </head>
 <body>
-	<div style="margin: 0 auto;">
+	<div class="catalog">
 		<?php
 
+$shop = new Shop('вибратор');
+$shop->makeProducts();
+
+// $a1 = new A();
+// $a2 = new A();
+// $a1->foo();
+// $a2->foo();
+// $a1->foo();
+// $a2->foo();
+
+// $a1 = new A();
+// $b1 = new B();
+// $a1->foo();
+// $b1->foo();
+// $a1->foo();
+// $b1->foo();
+
+$a1 = new A;
+$b1 = new B;
+$a1->foo();
+$b1->foo();
+$a1->foo();
+$b1->foo();
 ?>
 	</div>
 </body>
