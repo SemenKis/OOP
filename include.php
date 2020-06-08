f8321861e71b9dda6a7f6edb95988569e03ff0de
Semen Kiselev <semyon.kiselyov.90@mail.ru>
2020-06-08 19:30:04 +0300

    добавление и улучшение некоторых свойств

--- include.php
+++ include.php
@@ -7,23 +7,32 @@ class Shop {
 
 	public static $products = [];
 
-	public function __construct($products) {
-		$this->product = $products;
+	public function __construct($status, $email, $name) {
+		$this->status = $status;
+		$this->email = $email;
+		$this->name = $name;
+	}
+
+	public function getInfo() {
+		echo "<div class='info'>";
+		echo "{$this->status}" . "<br>" . "{$this->email}" . "<br>" . "{$this->name}";
+		echo "</div>";
 	}
 
 	public function makeProducts() {
 		$products = [
-			1 => ["id" => 1, "name" => "Микрофон"],
-			2 => ["id" => 2, "name" => "подставка"],
-			3 => ["id" => 3, "name" => "стойка"],
-			4 => ["id" => 4, "name" => "виски"],
-			5 => ["id" => 5, "name" => "кола"],
+			1 => ["id" => 1, "name" => "Микрофон", "cost" => "1200"],
+			2 => ["id" => 2, "name" => "подставка", "cost" => "1200"],
+			3 => ["id" => 3, "name" => "стойка", "cost" => "1200"],
+			4 => ["id" => 4, "name" => "виски", "cost" => "1200"],
+			5 => ["id" => 5, "name" => "кола", "cost" => "1200"],
 		];
 
 		foreach ($products as $items => $values) {
 			echo "<div class='product'>";
 			echo "<div id='id'>{$products[$items]["id"]}</div>";
 			echo "<div class='item'>{$products[$items]["name"]}</div>";
+			echo "<div id='cost'>{$products[$items]["cost"]}</div>";
 			echo "</div>";
 			echo "<br>";
 		}
@@ -31,7 +40,22 @@ class Shop {
 	}
 }
 
-// №5
+/**
+ *
+ */
+class User extends Shop {
+
+	public function rights() {
+
+		if ($status !== 'moderator') {
+			echo "у вас нет прав ";
+		} else {
+			echo "Добро пожаловать";
+		}
+	}
+}
+
+// №5.
 // class A {
 // 	public function foo() {
 // 		static $x = 0;
@@ -39,14 +63,13 @@ class Shop {
 // 	}
 // }
 
-// №5.2
+// №6
 // class A {
 // 	public function foo() {
 // 		static $x = 0;
 // 		echo ++$x;
 // 	}
 // }
-
 // class B extends A {
 // }
 
@@ -59,5 +82,4 @@ class A {
 }
 class B extends A {
 }
-
 ?>
